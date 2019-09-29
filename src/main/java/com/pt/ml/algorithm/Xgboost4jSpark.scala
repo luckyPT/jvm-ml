102fa197f0d1762aebe813e156ecdc4c34f0c3da
panteng <panteng@xiaomi.com>
2019-09-29 17:08:13 +0800

    xgboost 训练优化，使用官网提供的DEMO

--- src/main/java/com/pt/ml/algorithm/Xgboost4jSpark.scala
+++ src/main/java/com/pt/ml/algorithm/Xgboost4jSpark.scala
@@ -1,67 +1,48 @@
 package com.pt.ml.algorithm
 
-import com.pt.ml.util.MultiClassEvaluation
-import ml.dmlc.xgboost4j.scala.spark.XGBoost
-import org.apache.spark.ml.feature.LabeledPoint
+import ml.dmlc.xgboost4j.scala.spark.XGBoostClassifier
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructType}
 
-import scala.collection.mutable
-import scala.collection.mutable.ListBuffer
-
-/**
-  * xgboost对于spark版本有要求，使用dataFrame存在兼容性问题，因此使用Rdd
-  */
 object Xgboost4jSpark {
     def main(args: Array[String]): Unit = {
         val spark = SparkSession.builder()
-                .master("local[8]")
-                //.config("spark.task.cpus", "8") 切记不可增加此配置
-                .getOrCreate()
-        spark.sparkContext.setLogLevel("INFO")
+                .master("local[8]").getOrCreate()
+        val schema = new StructType(Array(
+            StructField("class", StringType, true),
+            StructField("sepal length", DoubleType, true),
+            StructField("sepal width", DoubleType, true),
+            StructField("petal length", DoubleType, true),
+            StructField("petal width", DoubleType, true)))
+        val rawInput = spark.read.schema(schema).csv("dataset/iris.csv")
 
-        val ministData = spark.read.format("csv").option("header", "true")
-                .load("/home/panteng/文档/dataset/MINIST/train.csv").rdd
-                .map {
-                    row =>
-                        val indeices = new ListBuffer[Int]()
-                        val values = new ListBuffer[Double]()
-                        var value = 0
-                        for (i <- Range(1, row.length)) {
-                            value = row.getAs[String](i).toInt
-                            if (value > 40) {
-                                indeices += i
-                                values += 1
-                            }
-                        }
-                        LabeledPoint(row.getAs[String](0).toDouble, org.apache.spark.ml.linalg.Vectors.sparse(row.length - 1, indeices.toArray, values.toArray))
-                }
+        import org.apache.spark.ml.feature.StringIndexer
+        val stringIndexer = new StringIndexer().
+                setInputCol("class").
+                setOutputCol("classIndex").
+                fit(rawInput)
+        val labelTransformed = stringIndexer.transform(rawInput).drop("class")
 
-        val Array(trainData, testData) = ministData.randomSplit(Array(0.7, 0.3))
-        val numRound = 10
-        val paramMap = List(
-            "booster" -> "gbtree",
-            "objective" -> "multi:softmax",
-            "num_class" -> 10,
+        import org.apache.spark.ml.feature.VectorAssembler
+        val vectorAssembler = new VectorAssembler().
+                setInputCols(Array("sepal length", "sepal width", "petal length", "petal width")).
+                setOutputCol("features")
+        val xgbInput = vectorAssembler.transform(labelTransformed).select("features", "classIndex")
+
+        val xgbParam = Map(
             "eta" -> 0.1f,
-            "gamma" -> 0.05,
-            "max_depth" -> 8,
-            "silent" -> 1
-        ).toMap
+            "missing" -> -999,
+            "objective" -> "multi:softprob",
+            "num_class" -> 3,
+            "num_round" -> 20,
+            "num_workers" -> 2
+        )
 
-        val model = XGBoost.trainWithRDD(trainData, paramMap, numRound, trainData.partitions.length)
-        print("train success")
-
-        val pre = model.predict(testData.map(l => l.features)).collect()
-        val testMinistData = testData.collect()
-        val preStatistics = mutable.ListBuffer[(Float, Float)]()
-        for (i <- pre.indices) {
-            val label = testMinistData(i).label
-            preStatistics.append((pre(i)(0), label.toFloat))
-        }
-        val preAndLabel = spark.sparkContext.parallelize(preStatistics).map {
-            case (f1, f2) =>
-                (f1.toDouble, f2.toDouble)
-        }
-        println(s" test accuracy:" + MultiClassEvaluation.accuracy(spark, preAndLabel))
+        val xgbClassifier = new XGBoostClassifier(xgbParam).
+                setFeaturesCol("features").
+                setLabelCol("classIndex")
+        val xgbClassificationModel = xgbClassifier.fit(xgbInput)
+        xgbClassificationModel.save("/home/panteng/IdeaProjects/jvm-ml/xgbModel")
+        spark.close()
     }
 }
